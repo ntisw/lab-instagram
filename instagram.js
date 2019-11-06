63e7e4d36c89bfc7266ce3c44213dc14c00818cf
Nantipat Suwanwong <scemisnis@gmail.com>
2019-11-06 20:46:15 +0700

    updated styles css

--- instagram.js
+++ instagram.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     //1.get instargram.json
-    $("#search_hashtag").keypress(function () {
+    $("#search_hashtag").click(function () {
         var hashtag = $("#hashtag").val()
         console.log(hashtag);
         $("#post").empty();
         $.get(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`, function (data, status) {
+       // $.get(`snap.json`, function (data, status) {
             console.log(status);
             if (status === 'success') {
-
                 //console.log(data);
                 var location = data.graphql.hashtag
                 var name = location.name
@@ -15,7 +15,7 @@ $(document).ready(function () {
                 var edge_location_to_top_posts = location.edge_hashtag_to_media
                 var count = edge_location_to_top_posts.count
                 $("#noofpost").text(count + ` posts`)
-
+                
                 var edges = edge_location_to_top_posts.edges
 
                 edges.forEach(element => {
@@ -27,10 +27,10 @@ $(document).ready(function () {
                     var liked = post.edge_liked_by.count
                     var caption = post.edge_media_to_caption.edges[0].node.text
                     var comments = post.edge_media_to_comment.count
-                    var item = `<div class="col-4" ><div style="background-image:url('${display_url}'); height:290px;
-                background-position: center; background-repeat: no-repeat; background-size: auto 100%; ">
+                    var item = `<div class="col-4" style="margin-bottom:60px;" ><div style="height:270px; background-image:url('${display_url}');  
+                background-position: center; background-repeat: no-repeat; background-size:  auto 100%; ">
                 </div><p><b>Liked</b>&nbsp;${liked} <b>Comments</b>&nbsp;${comments}</p>
-                <div class="card" style=" width:100%; border:none;">${caption}</div></div>`
+                <div class="overflow-auto" style="height: 100px; width:100%;">${caption}</div></div>`
                     $("#post").append(item)
                 });
 
